12ea17c6b9b1f96922c6763b1dd4134ba1c6c75b
Brayden-V1 <mitchellbl25@uww.edu>
2017-04-22 13:34:46 -0500

    Weekend Work 4/22 - More blanks to fill
    - Added P5.play master library to /Library Source Code + /libraries
    folders.
    - Added additional lines of code, including gameMouse() function. (with
    empty if statement)
    - Added region variables.
    - Added animation variables, to hold animations w/ p5.play.
    -  Added empty switch statement to gameControl().
    - Plugged heyListen() into preLoad.
    - Added walkerCount, conjPlaying, animCur, & towerReg variables.
    - Added gameStart, gameEnd, gamePause, gameResume functions - use with
    event listeners to control game flow + stop/reset the game.

--- Project Files/main.js
+++ Project Files/main.js
@@ -12,11 +12,15 @@
 //Variable Initialization
 var framerate; //Settings
 var dayLength, currentTime, currentScore; //Gameplay 
-var interBox, interCount, walkerMax, animTracker; //Game Control Variables
-var boxLock, gameGoing, idlePlaying; //Control Booleans
+var interBox, interCount, walkerCount, walkerMax, animTracker, animCur; //Game Control Variables
+var boxLock, gameGoing, idlePlaying, conjPlaying; //Control Booleans
+var towerReg; //Regions
+var fladIdle1, fladIdle2, fladConjure, walk1, walk2, walk3; //Animations
 
 function preLoad() {
+	//Preload animations + sprites here.
 
+	heyListen();
 }
 
 function setup() {
@@ -40,19 +44,26 @@ function setup() {
     textStyle(NORMAL);
 
     // Other Settings 
+    updateSprites(true); // Can pause sprites + animations. 
 
     //Variable Declarations
-    dayLength = 120.0; //2 Minutes
-    currentTime = 0.0;
-    currentScore = 0;
-    interBox = 2.0; 
-    walkerMax = 10; 
-    animTracker = 0.0; // Keeps track of idle time/tracks when to play an idle animation (in theory)
+    dayLength = 120.0; 	// 2 Minutes - length of playtime in seconds.
+    currentTime = 0.0; 	// Counts up to dayLength - currentt play session's length in seconds.
+    currentScore = 0; 	// Keeps track of score. 
+    interBox = 2.0; 	// Buffer time in seconds between each box drop.
+    walkerCount = 0;	// Keeps track of number of NPCs on screen.
+    walkerMax = 10; 	// Max number of NPCs on screen at once. 
+    animTracker = 0.0; 	// Keeps track of idle time/tracks when to play an idle animation (in theory)
+    animCur = 0; 		// Keeps track of which animation is currently playing. 
 
     //Booleans
-    boxLock = false; 
-    gameGoing = false;
-    idlePlaying = false;
+    boxLock = false; 	// When on, prevents player from conjuring boxes. (used to prevent spam)
+    gameGoing = false;	// When on, currentTime counts up, and play is in session. (for score/game control)
+    idlePlaying = false;// When on, an Idle animation is playing.
+    conjPlaying = false;// When on, the Conjure animation is playing. (used to prevent animation overlap)
+
+    //Gameplay Regions
+    towerReg = createVector();
 }
 
 function draw() {
@@ -60,6 +71,7 @@ function draw() {
 
 	gameTimer();
 	gameControl();
+	gameMouse();
 }
 
 function gameTimer() { //Constantly count up from current time until it reaches dayLength. Then, stop the game. 
@@ -86,20 +98,65 @@ function gameControl() { //Various settings for game control/balance. Stuff like
 			boxLock = false;
 		}
 	}
-
 	//Walker Control
-
+		// Spawns and controls the number of Walkers on screen. Keeps track of walkerCount. 
 
 	//Idle/Animation Control 
+	switch (animCur) {
+		case 0: //Idle 1 Animation
+				break;
+		case 1: //Idle 2 Animation
+				break;
+		case 2: //Conjure Animation
+				break; 
+		default: //Idle 1 Animation
+				break;
+	}
+}
 
+function gameMouse() { //Contains all mouse control functions.
+	if (mouseDown && !!(mouse.x <= towerReg.x && mouse.x >= towerReg.w && //Not sure if !! is the right tool to use here, to convert this statement into a boolean. - Brayden
+					  mouse.y <= towerReg.y && mouse.y >= towerReg.h) == true) //If the mouse isn't inside of the region set aside for the tower and/or UI, do this:
+	{
+		//Create box at mouse.x & mouse.y. 
+		//Wait for 0.5 seconds
+		//Then turn on gravity
+	}
 }
 
 function heyListen() {
 	//Any Event Listeners we might want go here. 
+
+	
 }
 
 //Event Listener functions go here. 
+function gameStart() { //Reset all variables, start game fresh. 
+	//Clear all Walkers currently in game. 
+	currentTime = 0.0;
+	currentScore = 0;
+	gameGoing = true;
+	boxLock = false;
+}
 
+function gamePause() { //Turns off gameGoing, which stops all gameplay. (stops currentTime, freezes all objects.)
+	gameGoing = false;
+	updateSprites(false);
+	// Freeze walkers and boxes in place. 
+
+	// (Idea) Alter CSS of menu objects like buttons, etc. to alter current buttons shown. - Brayden
+}
+
+function gameResume() { //Turns on gameGoing, resuming all gameplay. Frees all objects.
+	gameGoing = true; 
+	updateSprites(true);
+	// Return velocity to all previously moving objects. 
+}
+
+function gameEnd() { // Turns off booleans, updateSprites, and stops all counting. 
+	gameGoing = false;
+	updateSprites(false);
+}
 
 //Collision Detection functions go here. 
 function boxHit(which) { //Depending on which Walker was hit, add Walker's point value to the total score. 
